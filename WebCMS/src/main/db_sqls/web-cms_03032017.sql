bb1124a37e9a077c3a498a8df7428e8569d79675
AndreiOstafciuc <andreiostafciuc@gmail.com>
2017-03-03 15:37:57 +0200

    Added db schema containing only the user and user_info table

---
+++ WebCMS/src/main/db_sqls/web-cms_03032017.sql
@@ -0,0 +1,57 @@
+/*
+SQLyog Community
+MySQL - 5.7.14-log : Database - web-cms
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`web-cms` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `web-cms`;
+
+/*Table structure for table `user_info` */
+
+DROP TABLE IF EXISTS `user_info`;
+
+CREATE TABLE `user_info` (
+  `id_user` int(11) NOT NULL,
+  `firstName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `phoneNumber` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `user_info` */
+
+insert  into `user_info`(`id_user`,`firstName`,`lastName`,`phoneNumber`,`email`) values (1,'Test1','Test11','123123123','test1@gmail.com');
+insert  into `user_info`(`id_user`,`firstName`,`lastName`,`phoneNumber`,`email`) values (2,'Test2','Test22','123123123','test2@gmail.com');
+insert  into `user_info`(`id_user`,`firstName`,`lastName`,`phoneNumber`,`email`) values (3,'Test2','test33','123123123','test3@gmail.com');
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `id_role` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`username`,`password`,`id_role`) values (1,'test1','test',1);
+insert  into `users`(`id`,`username`,`password`,`id_role`) values (2,'test2','test',2);
+insert  into `users`(`id`,`username`,`password`,`id_role`) values (3,'test3','test',3);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
